679ed3e9cc4cb4a8d48de33623696d4a93a1ef71
Jerry Doran <Jerry.Doran@pccairfoils.com>
2019-08-27 12:10:07 -0400

    Refactored main.js for opening and closing modals

--- js/main.js
+++ js/main.js
@@ -1,76 +1,50 @@
-// TODO: Refactor this code to set up arrays to handle button click events
+const modals = document.querySelectorAll('.modal');
+const modalButtons = document.querySelectorAll('#modalButton');
+const closeButtons = document.querySelectorAll('.close-button');
 
-const modal = document.getElementById('simpleModal');
-const modal2 = document.getElementById('simpleModal2');
-const modal3 = document.getElementById('simpleModal3');
-const modal4 = document.getElementById('simpleModal4');
-const modalButton = document.getElementById('modalButton');
-const modalButton2 = document.getElementById('modalButton2');
-const modalButton3 = document.getElementById('modalButton3');
-const modalButton4 = document.getElementById('modalButton4');
-const closeBtn = document.querySelector('.close-btn');
-const closeBtn2 = document.querySelector('.close-btn2');
-const closeBtn3 = document.querySelector('.close-btn3');
-const closeBtn4 = document.querySelector('.close-btn4');
+window.addEventListener('click', () => {
+  modals.forEach(modal => {
+    modal.addEventListener('click', () => {
+      let id = modal.dataset.id;
+      clickOutside(id);
+    });
+  });
+});
 
-modalButton.addEventListener('click', openModal);
-modalButton2.addEventListener('click', openModal2);
-modalButton3.addEventListener('click', openModal3);
-modalButton4.addEventListener('click', openModal4);
+closeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    let id = button.dataset.id;
+    closeModal(id);
+  });
+});
 
-closeBtn.addEventListener('click', closeModal);
-closeBtn2.addEventListener('click', closeModal2);
-closeBtn3.addEventListener('click', closeModal3);
-closeBtn4.addEventListener('click', closeModal4);
+modalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    let id = button.dataset.id;
+    openModal(id);
+  });
+});
 
-window.addEventListener('click', clickOutside);
-window.addEventListener('click', clickOutside2);
-window.addEventListener('click', clickOutside3);
-window.addEventListener('click', clickOutside4);
-
-function openModal() {
-  modal.style.display = 'block';
-}
-function openModal2() {
-  modal2.style.display = 'block';
-}
-function openModal3() {
-  modal3.style.display = 'block';
-}
-function openModal4() {
-  modal4.style.display = 'block';
+function openModal(id) {
+  modals.forEach(modal => {
+    if (modal.dataset.id === id) {
+      modal.style.display = 'block';
+    }
+  });
 }
 
-function closeModal() {
-  modal.style.display = 'none';
-}
-function closeModal2() {
-  modal2.style.display = 'none';
-}
-function closeModal3() {
-  modal3.style.display = 'none';
-}
-function closeModal4() {
-  modal4.style.display = 'none';
+function closeModal(id) {
+  modals.forEach(modal => {
+    if (modal.dataset.id === id) {
+      modal.style.display = 'none';
+    }
+  });
 }
 
-function clickOutside(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-function clickOutside2(e) {
-  if (e.target === modal2) {
-    modal2.style.display = 'none';
-  }
-}
-function clickOutside3(e) {
-  if (e.target === modal3) {
-    modal3.style.display = 'none';
-  }
-}
-function clickOutside4(e) {
-  if (e.target === modal4) {
-    modal4.style.display = 'none';
-  }
+function clickOutside(id) {
+  modals.forEach(modal => {
+    if (modal.dataset.id === id) {
+      modal.style.display = 'none';
+    }
+  });
 }
